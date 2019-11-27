9cab1c66b7c676aefd78a965d51b3fed3e4498bc
UrsusArctos <UrsusArctos@github.com>
2019-11-28 01:15:18 +0200

    EPUB output + minor additions to 1 and 2 chaps

--- build.sh
+++ build.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
 #
-rm *.aux
-rm *.log
-rm *.out
-rm *.pdf
-rm *.toc
+function CleanTrash () {
+	# Junk files left out after processing
+	rm *.aux
+	rm *.log
+	rm *.out
+	rm *.toc
+	rm *.opf
+	rm *.4ct
+	rm *.4tc
+	rm *.css
+	rm *.dvi
+	rm *.html
+	rm *.idv
+	rm *.lg
+	rm *.ncx
+	rm *.tmp
+	rm *.xref
+	# Junk directories left out after processing
+	rm -rf tdd-epub
+}
 #
-pdflatex tdd
-pdflatex tdd
+# Build targets
+rm *.pdf
+rm *.epub
+#
+CleanTrash
+#
+pdflatex -jobname=TheDarkDefeat tdd
+pdflatex -jobname=TheDarkDefeat tdd
+#
+CleanTrash
+#
+tex4ebook tdd
+mv tdd.epub TheDarkDefeat.epub
+#
+CleanTrash
 #
